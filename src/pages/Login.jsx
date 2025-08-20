2d3089a027a689939409038d35edcda6ea9d0bd8
AndrewLG03 <loaiza29andres@gmail.com>
2025-08-20 15:15:50 -0600

    Prueba login 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await fetch('https://juth-works-84sbea6lk-andres-loaiza-guzmans-projects.vercel.app/api/login', {
+      const response = await fetch('https://juthworksbackend.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
