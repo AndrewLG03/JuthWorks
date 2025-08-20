24c680634cc09da94154fb69a1c5628a2edf0c02
AndrewLG03 <loaiza29andres@gmail.com>
2025-08-20 15:59:08 -0600

    regreso al fix2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await fetch('https://juthworksbackend.onrender.com/api/login', {
+      const response = await fetch('http://localhost:5000/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
